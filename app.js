5b97049a0334e44c527590a57dbb19cd4002a4da
Sean Tumulak-Nguyen <seantumulaknguyen@gmail.com>
2024-05-13 20:42:12 -0400

    init commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter Message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"></body>'
+    );
+    res.write("</html>");
+  }
+
+  if (url === "/message") {
+  }
+});
