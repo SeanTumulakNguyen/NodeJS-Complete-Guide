e22f9394fafb59b70ef782b1ef94a767d769ef8b
Sean Tumulak-Nguyen <seantumulaknguyen@gmail.com>
2024-05-13 20:54:27 -0400

    add gitignore to prevent txt files from being pushed

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"></body>'
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></body>'
     );
     res.write("</html>");
     return res.end();
@@ -22,7 +22,7 @@ const server = http.createServer((req, res) => {
       body.push(chunk);
     });
     req.on("end", () => {
-      const parseBody = Buffer.concat(body).toString();
+      const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       fs.writeFileSync("message.txt", message);
     });
