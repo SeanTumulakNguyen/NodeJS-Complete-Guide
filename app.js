ef5c1bb776f2f17a6d199b23967b730e3373483b
sean <seantumulaknguyen@gmail.com>
2024-07-08 23:40:16 -0400

    adding bodyparser along with app options

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const http = require("http");
 const express = require("express");
+const bodyParse = require("body-parser");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use("/add-product", (request, response, next) => {
-  console.log("In the middleware");
   response.send("<h1>Add product page</h1>");
 });
 
+app.post("/product", (request, response, next) => {
+  console.log(request.body);
+  response.redirect("/");
+});
+
 app.use("/", (request, response, next) => {
-  console.log("In the middleware");
   response.send("<h1>Hello from express</h1>");
 });
 
