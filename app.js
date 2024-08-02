30d4bbdc19cf06c1d6336aed0b20477f8d22f173
Sean Tumulak-Nguyen <seantumulaknguyen@gmail.com>
2024-08-01 20:25:25 -0400

    404 page and root directory helper

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const path = require("path");
 const express = require("express");
 const bodyParse = require("body-parser");
 const bodyParser = require("body-parser");
@@ -8,13 +9,15 @@ const app = express();
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
+const rootDir = require("./util/path");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page note found</h1>");
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const path = require("path");
 
+const rootDir = require("../util/path");
+
 const router = express.Router();
 
 router.get("/add-product", (request, response, next) => {
-  response.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  response.sendFile(path.join(rootDir, "../", "views", "add-product.html"));
 });
 
 router.post("/add-product", (request, response, next) => {
