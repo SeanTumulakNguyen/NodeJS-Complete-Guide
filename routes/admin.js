5e7e0116418c2978f322a1cb0f790ed4b7f7484d
Sean Tumulak-Nguyen <seantumulaknguyen@gmail.com>
2024-08-01 21:16:46 -0400

    update files to project start of new module

--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,16 @@
 const express = require("express");
 const path = require("path");
-
 const rootDir = require("../util/path");
 
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get("/add-product", (request, response, next) => {
   response.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 
+// /admin/add-product => POST
 router.post("/add-product", (request, response, next) => {
-  console.log(request.body);
   response.redirect("/");
 });
 
